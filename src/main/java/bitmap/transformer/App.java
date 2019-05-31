f32f05dd7aaede3ca763987d6d3bcc98b52234e5
Will Fritts <waf515@gmail.com>
2019-05-31 11:30:11 -0700

    wrote dot matrix method

--- src/main/java/bitmap/transformer/App.java
+++ src/main/java/bitmap/transformer/App.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 public class App {
     String filePath = "./src/main/resources/LAND2.BMP";
+
     public String transformImageByColor(int rgbTobeChangedTo) {
         BufferedImage img = null;
         try {
@@ -32,10 +33,62 @@ public class App {
         return "image";
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void transformImgDotMatrix(int rgbToSetDotMatrix){
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(filePath));
+            int height = img.getHeight();
+            int width = img.getWidth();
+
+            for(int h = 0; h < height; h++){
+                for(int w = 0; w < width; w++){
+                    if(h % 10 == 0 && w % 10 == 0){
+                        img.setRGB(w, h, rgbToSetDotMatrix);
+                    }
+                }
+            }
+            ImageIO.write(img, "BMP", new File("./src/main/resources/dotMatrix.bmp"));
+        } catch(IOException e){
+
+        }
+        System.out.println(img);
+    }
+
+
+
     
 
     public static void main(String[] args) {
-        System.out.println(new App().transformImageByColor(255));
+
+
+        
+        new App().transformImgDotMatrix(0);
     }
 
 }
